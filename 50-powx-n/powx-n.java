709645daa44c4f6ee8f7066c1beeed7e1c1b2f02
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-06-15 14:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (10.73%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,21 @@
 class Solution {
+    static double power(double x, long n) {
+
+    if(n == 0)
+        return 1;
+
+    if(n < 0)
+        return 1 / power(x, -n);
+
+    if(n % 2 == 0) {
+        return power(x * x, n / 2);
+    }
+    else {
+        return x * power(x, n - 1);
+    }
+}
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
-        
+        double j=power(x,n);
+        return j;
     }
 }
