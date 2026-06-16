e1bba9cb9691cb98cfca3ee710cd2415b9bda729
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-06-16 18:16:49 +0530

    Time: 2 ms (19.37%) | Memory: 46.8 MB (41.18%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -27,7 +27,7 @@ class Solution {
         
         while (!q.isEmpty()) {
             
-            int size = q.size();   // nodes in current level
+            int size = q.size();
             List<Integer> level = new ArrayList<>();
             
             for (int i = 0; i < size; i++) {
