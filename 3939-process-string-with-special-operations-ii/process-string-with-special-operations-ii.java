0592c6ab229abf579aee060b2d3c1b93d2457240
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-06-17 18:23:29 +0530

    Time: 28 ms (88.46%) | Memory: 48.1 MB (15.38%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public char processStr(String s, long k) {
+
+        long len = 0;
+
+        for(char c : s.toCharArray()){
+            if(c=='*'){
+                if(len>0) len--;
+            }
+            else if(c=='#'){
+                len *= 2;
+            }
+            else if(c!='%'){
+                len++;
+            }
+        }
+
+        if(k >= len) return '.';
+
+        for(int i=s.length()-1;i>=0;i--){
+            char c=s.charAt(i);
+
+            if(c=='#'){
+                len /= 2;
+                if(k >= len)
+                    k -= len;
+            }
+            else if(c=='%'){
+                k = len - 1 - k;
+            }
+            else if(c=='*'){
+                len++;
+            }
+            else{
+                len--;
+                if(k == len)
+                    return c;
+            }
+        }
+
+        return '.';
+    }
+}
