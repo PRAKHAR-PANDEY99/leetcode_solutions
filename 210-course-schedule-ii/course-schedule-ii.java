f2b45ff7024a118f4e67d676c5b62fedb8c97f1d
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-07-29 22:31:30 +0530

    Time: 6 ms (42.70%) | Memory: 46.9 MB (52.67%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,77 +1,40 @@
-import java.util.*;
-
 class Solution {
-
-    boolean dfs(int node, ArrayList<ArrayList<Integer>> adj,
-                boolean[] vis, boolean[] pathVis,
-                ArrayList<Integer> ans) {
-
-        vis[node] = true;
-        pathVis[node] = true;
-
-        for (int it : adj.get(node)) {
-
-            if (!vis[it]) {
-                if (dfs(it, adj, vis, pathVis, ans))
-                    return true;
-            }
-            else if (pathVis[it]) {
-                return true; // cycle exists
-            }
-        }
-
-        // store after completing all dependencies
-        ans.add(node);
-
-        pathVis[node] = false;
-
-        return false;
-    }
-
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i < numCourses; i++) {
+        int[] incoming=new int[numCourses];
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int j=0;j<numCourses;j++){
             adj.add(new ArrayList<>());
         }
-
-
-        // prerequisite -> course
-        for (int[] edge : prerequisites) {
+        for(int[] edge : prerequisites)
+        {
             adj.get(edge[1]).add(edge[0]);
         }
-
-
-        boolean[] vis = new boolean[numCourses];
-        boolean[] pathVis = new boolean[numCourses];
-
-        ArrayList<Integer> ans = new ArrayList<>();
-
-
-        for (int i = 0; i < numCourses; i++) {
-
-            if (!vis[i]) {
-
-                if (dfs(i, adj, vis, pathVis, ans)) {
-                    return new int[0]; // cycle exists
-                }
-
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            for(Integer it: adj.get(i)){
+                incoming[it]++;
             }
         }
-
-
-        // reverse because DFS gives reverse topological order
-        Collections.reverse(ans);
-
-
-        int[] result = new int[numCourses];
-
-        for (int i = 0; i < numCourses; i++) {
-            result[i] = ans.get(i);
+        int[] ans=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(incoming[i]==0){
+                q.offer(i);
+            }
         }
-
-        return result;
+        int index=0;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            ans[index++]=node;
+            for(int it:adj.get(node)){
+                incoming[it]--;
+                if(incoming[it]==0){
+                    q.offer(it);
+                }
+            }
+            
+        }
+        if(index==numCourses) return ans;
+        return new int[0];
+        
     }
 }
