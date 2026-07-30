b734f37ec593f7f963b24dfedf94feb1e46b36da
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-07-30 12:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (99.04%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -5,10 +5,13 @@ class Solution {
         int a=len/8;
         int rem=len%8;
         int ans=0;
-        for(int i=1;i<=a;i++){
-            ans=ans+(8*i);
-        }
+        // for(int i=1;i<=a;i++){
+        //     ans=ans+(8*i);
+        // }
+        // ans=ans+(rem*(a+1));
+        // return ans;  
+        ans=(8*a*(a+1))/2;
         ans=ans+(rem*(a+1));
-        return ans;  
+        return ans;
     }
 }
