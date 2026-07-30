90cf9f630f0a78b7b4dbde81972521f825eef987
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-07-30 12:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (95.69%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int len=word.length();
+        if(len<=8) return len;
+        int a=len/8;
+        int rem=len%8;
+        int ans=0;
+        for(int i=1;i<=a;i++){
+            ans=ans+(8*i);
+        }
+        ans=ans+(rem*(a+1));
+        return ans;  
+    }
+}
