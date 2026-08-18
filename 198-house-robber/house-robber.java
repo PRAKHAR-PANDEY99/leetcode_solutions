9ee56bb0f7a1a85ba826fcd1797a0853793157da
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-08-18 22:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.90%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,18 @@
 class Solution {
-    int  fn(int ind,int[] nums,int[] dp){
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int pic=nums[ind]+fn(ind-2,nums,dp);
-        int notpic=fn(ind-1,nums,dp);
-        return dp[ind]=Math.max(pic,notpic);
-    }
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length+1];
-        Arrays.fill(dp, -1);
-        int sum=fn(nums.length-1,nums,dp);
-        dp[0]=nums[0];
-        
-        return sum;
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<nums.length;i++){
+            int take=nums[i];
+            if(i>1){
+                take=take+prev2;
+            }
+            int nottake=0+prev;
+            int curr=Math.max(take,nottake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
         
     }
 }
