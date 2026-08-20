04e6a7f5d90b6e3f322353a74d49b972d336dcbc
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-08-21 00:57:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (81.65%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,42 +1,45 @@
 class Solution {
-    int fn(int m,int n,int[][] dp){
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-        int row=m-1;
-        int col=n-1;
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        int left=fn(m,col,dp);
-        if(m>=0 && col>=0){
-            dp[m][col]=left;
-        }
-        int right=fn(row,n,dp);
-        if(row>=0 && n>=0){
-            dp[row][n]=right;
-        }
-        dp[m][n]=left+right;
-        return dp[m][n];
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if (obstacleGrid[0][0] == 1) {
     return 0;
-}
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        for(int i=0;i<obstacleGrid.length;i++){
-            for(int j=0;j<obstacleGrid[0].length;j++){
+}      
+       int m=obstacleGrid.length;
+       int n=obstacleGrid[0].length;
+       int[] prev = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            int[] curr = new int[n];
+
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0) {
+                    curr[j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                if (i > 0) {
+                    up = prev[j];
+                }
+
+                if (j > 0) {
+                    left = curr[j - 1];
+                }
                 if(obstacleGrid[i][j]==1){
-                    dp[i][j]=0;
+                    curr[j]=0;
+                }
+                else{
+                    curr[j] = up + left;
+
                 }
             }
+
+            prev = curr;
         }
-        int ans=fn(m-1,n-1,dp);
-        return ans;
-        
+
+        return prev[n - 1];
+
     }
 }
