7476594edc4e7400378feedc6b3c4d9afe7517c7
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-09-07 16:10:05 +0530

    Time: 50 ms (49.19%) | Memory: 47.6 MB (55.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,35 +1,28 @@
 class Solution {
-    boolean sum(int[] nums,int i,int target,int[][] dp){
-        if(target==0) return true;
-        if(i==0) return nums[i]==target;
-        boolean t=false;
-        boolean nt=false;
-        if(dp[i][target]!=-1){
-            return dp[i][target]==1;
-        }
-        nt=sum(nums,i-1,target,dp);
-        if(target>=nums[i]){
-            t=sum(nums,i-1,target-nums[i],dp);
-        }
-        dp[i][target]=(t || nt) ? 1:0;
-        return (t || nt);
-    }
     public boolean canPartition(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum=sum+nums[i];
         }
-        if(sum%2!=0){
-            return false;
-        }
-        int[][] dp=new int[nums.length][sum/2+1];
+        if(sum%2!=0) return false;
         int target=sum/2;
+        boolean[][] dp=new boolean[nums.length][target+1];
         for(int i=0;i<nums.length;i++){
-            Arrays.fill(dp[i],-1);
+            dp[i][0]=true;
         }
-        boolean ans=sum(nums,nums.length-1,target,dp);
-        return ans;
-
-        
+        if(nums[0]<=target){
+            dp[0][nums[0]]=true;
+        }
+        for(int i=1;i<nums.length;i++){
+            for(int targett=1;targett<=target;targett++){
+                boolean Nottake=dp[i-1][targett];
+                boolean take=false;
+                if(nums[i]<=targett){
+                    take=dp[i-1][targett-nums[i]];
+                }
+                dp[i][targett]=Nottake || take;
+            }
+        }
+        return dp[nums.length-1][target];
     }
 }
