8bb9bde20ce1db4ae64501321da54a5a933e095c
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-09-08 16:33:46 +0530

    Time: 7 ms (65.57%) | Memory: 46.8 MB (23.19%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,35 @@
 class Solution {
-    int sum(int[] nums,int target,int i){
-        if(i==nums.length && target==0){
-            return 1;
-        }
-        if(i==nums.length){
+    int sum(int[] nums, int target, int i, int[][] dp, int offset) {
+        if(i==nums.length-1){
+            if(target==0 && nums[i]==0) return 2;
+            else if(target-nums[i]==0 || target+nums[i]==0) return 1;
             return 0;
         }
-        int takePositive=0;
-        int takeNegative=0;
-        takePositive=sum(nums,target-nums[i],i+1);
-        takeNegative=sum(nums,target+nums[i],i+1);
-        return takePositive+takeNegative;
+        int index = target + offset;
+        if (dp[i][index] != Integer.MIN_VALUE) {
+            return dp[i][index];
+        }
+        int takePositive = 0;
+        int takeNegative = 0;
+        takePositive = sum(nums, target - nums[i], i + 1, dp, offset);
+        takeNegative = sum(nums, target + nums[i], i + 1, dp, offset);
+        return dp[i][index] = takePositive + takeNegative;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int ans=sum(nums,target,0);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+        }
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+        int offset = sum-target;
+        int[][] dp = new int[nums.length][2 * sum + 1];
+        for (int i = 0; i < nums.length; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+        int ans = sum(nums, target, 0, dp, offset);
         return ans;
-        
     }
 }
+
