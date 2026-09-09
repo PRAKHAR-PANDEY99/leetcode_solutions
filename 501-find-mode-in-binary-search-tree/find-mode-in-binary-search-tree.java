a167308c312f4a57af5ecaafd52e4e72f446f2ba
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-09-09 11:05:19 +0530

    Time: 2 ms (58.25%) | Memory: 47.4 MB (13.68%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -1,34 +1,52 @@
 class Solution {
-    void dfs(TreeNode root, HashMap<Integer, Integer> map) {
-        if (root == null) return;
-        dfs(root.left, map);
-        if (!map.containsKey(root.val)) {
-            map.put(root.val, 1);
-        } else {
-            map.put(root.val, map.get(root.val) + 1);
-        }
-        dfs(root.right, map);
+    int prev = Integer.MAX_VALUE;
+int count = 0;
+int maxFreq = 0;
+    void dfs(TreeNode root, List<Integer> list) {
+    if (root == null) return;
+    dfs(root.left, list);
+    if (prev != Integer.MAX_VALUE && prev == root.val) {
+        count++;
+    } else {
+        count = 1;
     }
+    if (count > maxFreq) {
+        maxFreq = count;
+        list.clear();
+        list.add(root.val);
+    } else if (count == maxFreq) {
+        list.add(root.val);
+    }
+    prev = root.val;
+    dfs(root.right, list);
+    return;
+}
     public int[] findMode(TreeNode root) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        dfs(root, map);
-        int max = 0;
-        for (int key : map.keySet()) {
-            max = Math.max(max, map.get(key));
-        }
-        int count = 0;
-        for (int key : map.keySet()) {
-            if (map.get(key) == max) {
-                count++;
-            }
-        }
-        int[] ans = new int[count];
-        int i = 0;
-        for (int key : map.keySet()) {
-            if (map.get(key) == max) {
-                ans[i++] = key;
-            }
-        }
-        return ans;
+        List<Integer> list=new ArrayList<>();
+        dfs(root,list);
+        int [] n=list.stream().mapToInt(Integer::intValue).toArray();
+        return n;
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // dfs(root, map);
+        // int max = 0;
+        // for (int key : map.keySet()) {
+        //     max = Math.max(max, map.get(key));
+        // }
+        // int count = 0;
+        // for (int key : map.keySet()) {
+        //     if (map.get(key) == max) {
+        //         count++;
+        //     }
+        // }
+        // int[] ans = new int[count];
+        // int i = 0;
+        // for (int key : map.keySet()) {
+        //     if (map.get(key) == max) {
+        //         ans[i++] = key;
+        //     }
+        // }
+        // return ans;
+
     }
 }
