7a2cdd20dab848b6a362dfedc3ef713cb81b342a
Prakhar Pandey <prakharpandey1309@gmail.com>
2026-09-19 18:49:37 +0530

    Time: 20 ms (45.02%) | Memory: 54.5 MB (33.91%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,36 +1,53 @@
 class Solution {
-    int sum(String s,String t,int i,int j,int[][] dp){
-        if(i<0 || j<0){
-            if(j<0){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int take1=0;
-        int take2=0;
-        int nottake=0;
-        if(s.charAt(i)==t.charAt(j)){
-            take1=sum(s,t,i-1,j-1,dp);
-            take2=sum(s,t,i-1,j,dp);
-            return  dp[i][j]=take1+take2;
-        }
-        else{
-            nottake=sum(s,t,i-1,j,dp);
-            return dp[i][j]=nottake;
-        }
-    }
+    // int sum(String s,String t,int i,int j,int[][] dp){
+    //     if(i<0 || j<0){
+    //         if(j<0){
+    //             return 1;
+    //         }
+    //         else{
+    //             return 0;
+    //         }
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int take1=0;
+    //     int take2=0;
+    //     int nottake=0;
+    //     if(s.charAt(i)==t.charAt(j)){
+    //         take1=sum(s,t,i-1,j-1,dp);
+    //         take2=sum(s,t,i-1,j,dp);
+    //         return  dp[i][j]=take1+take2;
+    //     }
+    //     else{
+    //         nottake=sum(s,t,i-1,j,dp);
+    //         return dp[i][j]=nottake;
+    //     }
+   // }
     public int numDistinct(String s, String t) {
-        int[][] dp=new int[s.length()][t.length()];
-        for(int i=0;i<s.length();i++){
-            Arrays.fill(dp[i],-1);
+        int n=s.length();
+        int m=t.length();
+        int[][] dp=new int[s.length()+1][t.length()+1];
+        // for(int i=0;i<s.length();i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        
+            for(int i=0;i<=n;i++){
+                dp[i][0]=1;
+            }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-        int ans=sum(s,t,s.length()-1,t.length()-1,dp);
-        return ans;
+        
+        return dp[n][m];
            
     }
 }
