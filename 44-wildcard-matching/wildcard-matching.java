1f4698f9c91f21cf81710e449d209b53c3be3b1a
Prakhar Pandey <prakharpandey1309@gmail.com>
2026-09-19 23:19:13 +0530

    Time: 54 ms (22.64%) | Memory: 47.4 MB (58.40%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,44 +1,34 @@
 class Solution {
-    boolean dfs(String s,String p,int i,int j,Boolean[][] dp){
-        if(j==p.length() && i==s.length()){
-                return true;
+    public boolean isMatch(String s,String p) {
+        int n=s.length();
+        int m=p.length();
+
+        boolean[][] dp=new boolean[n+1][m+1];
+
+        dp[0][0]=true;
+
+        for(int j=1;j<=m;j++){
+            if(p.charAt(j-1)=='*'){
+                dp[0][j]=dp[0][j-1];
             }
-        if(j==p.length() && i!=s.length()){
-            return false;
-        }
-        if(i == s.length()){
-    for(int k=j;k<p.length();k++){
-        if(p.charAt(k) != '*'){
-            return false;
-        }
-    }
-    return true;
-}
-        if(dp[i][j]!=null){
-            return dp[i][j];
-        }
-        boolean match=false;
-        boolean not2=false;
-        boolean not3=false;
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
-            match=dfs(s,p,i+1,j+1,dp);
-            return dp[i][j]=match;
-        }
-        else if(p.charAt(j)=='*'){
-            not2=dfs(s,p,i,j+1,dp);
-            not3=dfs(s,p,i+1,j,dp);
-            return  dp[i][j]=not2 || not3;
-
-        }
-        else{
-            return dp[i][j]=false;
         }
 
-    }
-    public boolean isMatch(String s, String p) {
-        Boolean[][] dp=new Boolean[s.length()][p.length()];
-        boolean ans=dfs(s,p,0,0,dp);
-        return ans;
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1)=='*'){
+                    boolean skip=dp[i][j-1];
+                    boolean take=dp[i-1][j];
+                    dp[i][j]=skip||take;
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
