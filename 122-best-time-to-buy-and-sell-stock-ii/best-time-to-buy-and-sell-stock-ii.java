fa343d8e0fecefed0cf309f71a3186c1031bb27e
Prakhar Pandey <prakharpandey1309@gmail.com>
2026-09-20 21:12:38 +0530

    Time: 2 ms (16.43%) | Memory: 46.4 MB (48.59%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,28 +1,42 @@
 class Solution {
-    int sum(int[] prices,int i,int buy,int[][] dp){
-        if(i==prices.length){
-            return 0;
-        }
-        if(dp[i][buy]!=-1){
-            return dp[i][buy];
-        }
-        int profit=0;
-        if(buy==1){
-            profit=Math.max(-prices[i]+sum(prices,i+1,0,dp),0+sum(prices,i+1,1,dp));
-        }
-        else{
-            profit=Math.max(prices[i]+sum(prices,i+1,1,dp),0+sum(prices,i+1,0,dp));
-        }
-        return dp[i][buy]=profit;
-    }
+    // int sum(int[] prices,int i,int buy,int[][] dp){
+    //     if(i==prices.length){
+    //         return 0;
+    //     }
+    //     if(dp[i][buy]!=-1){
+    //         return dp[i][buy];
+    //     }
+    //     int profit=0;
+    //     if(buy==1){
+    //         profit=Math.max(-prices[i]+sum(prices,i+1,0,dp),0+sum(prices,i+1,1,dp));
+    //     }
+    //     else{
+    //         profit=Math.max(prices[i]+sum(prices,i+1,1,dp),0+sum(prices,i+1,0,dp));
+    //     }
+    //     return dp[i][buy]=profit;
+    // }
 
     public int maxProfit(int[] prices) {
-        int [][] dp=new int[prices.length][2];
-        for(int i=0;i<prices.length;i++){
-            Arrays.fill(dp[i],-1);
+        int n=prices.length;
+        int [][] dp=new int[prices.length+1][2];
+        dp[prices.length][0]=0;
+        dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                int profit=0;
+                if(buy==1){
+            profit=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
         }
-        int ans=sum(prices,0,1,dp);
-        return ans;
+        else{
+            profit=Math.max(
+                        prices[i] + dp[i + 1][1],
+                        dp[i + 1][0]
+                    );
+        }
+        dp[i][buy]=profit;
+            }
+        }
+        return dp[0][1];
         
     }
 }
